eaba1b2747fecee083aa3a148b549b1e61a69611
akak1y <gamer.all.games@list.ru>
2026-08-16 21:51:10 +0300

    refactor: клиент — авторизация вынесена в auth.js

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,5 +1,6 @@
 require('./state'); // создаём UIstate
 const state = globalThis.UIState;
+require('./auth');
 
 mp.gui.chat.show(false); // скрываем чат и миникарту
 mp.game.ui.displayRadar(false);
@@ -50,32 +51,6 @@ mp.events.add("browserCreated", (browser) => { // когда создался б
     if (state.uiBrowser && browser === state.uiBrowser){
         mp.gui.cursor.show(true, true) // включаем курсор для авторизации
     }
-});
-
-mp.events.add("client:account:submitLogin", (username, password) => { // кнопка войти
-    mp.events.callRemote("server:account:login", username, password) // пересылаем на сервер для проверки
-});
-
-mp.events.add("client:account:authError", (msg) => { // ошибка авторизации
-    if (state.uiBrowser) state.uiBrowser.execute(`window.showAuthError("${msg}");`); 
-});
-
-mp.events.add("client:account:hideAuth", (developer) => { // успешная авторизация
-    mp.gui.cursor.show(false, false); // сбрасываем все блокировки при спавне
-    mp.gui.chat.show(true);
-    mp.game.ui.displayRadar(true);
-    state.isAuthorized = true;
-    state.globalKeyBlock = false;
-    state.isAnyUiWindowOpen = false;
-    state.openWindowsState = { inventory: false, phone: false, dealership: false, carCustom: false };
-    mp.events.callRemote("server:dealership:requestPos");
-    mp.events.callRemote("server:garage:requestPos");
-    mp.events.callRemote("server:customCar:requestPos");
-    mp.events.callRemote("server:fuel:requestPos");
-    mp.events.callRemote("server:courier:requestPos");
-    mp.events.callRemote("server:phone:requestPriceDeliveryCar");
-    if (state.uiBrowser) state.uiBrowser.execute(`window.changeScreen("game");`); // меняем окно авторизации на игровой худ
-    state.playerIsDeveloper = developer
 });
 
 mp.events.add("client:ui:windowStateChanged", (winName, isOpen) => { // выключаем/включаем чат при открытии/закрытии любого окна
